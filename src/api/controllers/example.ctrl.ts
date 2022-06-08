1a950e01a51de4222fc473f027a7740621639bc0
Pablo Aragón <pablo.aragon22@gmail.com>
2022-06-08 10:51:37 +0200

    feat: include service

--- src/api/controllers/example.ctrl.ts
+++ src/api/controllers/example.ctrl.ts
@@ -1,5 +1,6 @@
 import Example from '../../models/example.model';
 import exampleRepo from '../../repository/example.repo';
+import exampleService from '../../services/example.service';
 import Controller from '../schema/Controller';
 import UpdateExampleRequest from '../schema/UpdateExampleRequest';
 import UpdateExampleResponse from '../schema/UpdateExampleResponse';
@@ -9,6 +10,9 @@ export default class ExampleController extends Controller {
     const id = parseInt(req.params.id, 10);
     const example: Example = req.body;
     example.id = id;
+
+    const avatar = await exampleService.getAvatar(id);
+    example.avatar = avatar;
 
     const result = await exampleRepo.saveExample(example);
 
--- src/services/example.service.ts
+++ src/services/example.service.ts
@@ -1,24 +1,12 @@
 import axios from 'axios';
 import axiosBetterStacktrace from 'axios-better-stacktrace';
-import Example from '../models/example.model';
 
 axiosBetterStacktrace(axios);
 
 export default {
-  async getExample(id: string): Promise<Example> {
-    const response = await axios.get<Example>(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  async getAvatar(id: number): Promise<string> {
+    const response = await axios.get<{ thumbnailUrl: string }>(`https://jsonplaceholder.typicode.com/photos/${id}`);
 
-    return response.data;
-  },
-
-  async addExample(example: Example): Promise<Example> {
-    const headers: any = {
-      'Content-type': 'application/json; charset=UTF-8',
-    };
-    const body = example;
-
-    const response = await axios.post<Example>('https://jsonplaceholder.typicode.com/posts', body, { headers });
-
-    return response.data;
+    return response.data.thumbnailUrl;
   },
 };
